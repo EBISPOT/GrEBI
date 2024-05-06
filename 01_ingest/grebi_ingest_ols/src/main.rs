5c1127771243fc3d58a93ccd41c526e14cc0b8ef
James McLaughlin <james@mclgh.net>
2024-05-06 15:52:48 +0100

    fix ols ingest not working with some ontologies

--- 01_ingest/grebi_ingest_ols/src/main.rs
+++ 01_ingest/grebi_ingest_ols/src/main.rs
@@ -168,25 +168,35 @@ fn read_entities(json: &mut JsonStreamReader<BufReader<StdinLock<'_>>>, output_n
                 let curie = get_string_values(obj.get("ols:curie").unwrap()).iter().next().unwrap().to_string();
                 let pref_prefix = {
                     if curie.contains(":") {
-                        curie.split(":").next().unwrap().to_ascii_lowercase()
+                        Some(curie.split(":").next().unwrap().to_ascii_lowercase())
                     } else {
-                        get_string_values(obj.get("ols:definedBy").unwrap()).iter().next().unwrap().to_string()
+			let definedBy = obj.get("ols:definedBy");
+			if definedBy.is_some() {
+				Some(get_string_values(definedBy.unwrap()).iter().next().unwrap().to_string())
+			} else {
+				None
+			}
                     }
                 };
-                let label = get_string_values(obj.get("ols:label").unwrap()).iter().next().unwrap().to_string();
+		if !pref_prefix.is_some() {
+			curie.to_string()
+		} else {
+			let pref_prefix_u = pref_prefix.unwrap().to_string();
+			let label = get_string_values(obj.get("ols:label").unwrap()).iter().next().unwrap().to_string();
 
-                // this might not be a real label, in which case just return the curie
-                if label.starts_with(&(pref_prefix.to_string() + ":")) || label.starts_with(&(pref_prefix.to_string() + "_")) {
-                    curie.to_string()
-                } else {
-                    pref_prefix.to_string() + ":" + &label.to_string().as_bytes().iter().map(|x| {
-                        if x.is_ascii_alphanumeric() {
-                            *x as char
-                        } else {
-                            '_'
-                        }
-                    }).collect::<String>()
-                }
+			// this might not be a real label, in which case just return the curie
+			if label.starts_with(&(pref_prefix_u.to_owned()  + ":")) || label.starts_with(&(pref_prefix_u.to_owned() + "_")) {
+			    curie.to_string()
+			} else {
+			    pref_prefix_u.to_string() + ":" + &label.to_string().as_bytes().iter().map(|x| {
+				if x.is_ascii_alphanumeric() {
+				    *x as char
+				} else {
+				    '_'
+				}
+			    }).collect::<String>()
+			}
+		}
             };
 
             output_nodes.write_all(r#"{"id":"#.as_bytes()).unwrap();
