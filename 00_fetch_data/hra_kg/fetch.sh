0ebbb51de5a8b94423dbb3693d92c764889e7e77
James McLaughlin <james@mclgh.net>
2024-06-11 19:50:27 +0100

    add hra fetch script

---
+++ 00_fetch_data/hra_kg/fetch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -f https://cdn.humanatlas.io/digital-objects/blazegraph.jnl
+wget https://cdn.humanatlas.io/digital-objects/blazegraph.jnl
+
+docker run --entrypoint /data/entrypoint.dockersh -v $(pwd):/data ghcr.io/ebispot/blazegraph-docker:2.1.5 
+
