f25ef70cc4b13de2cee7597033d170e73a0176f9
James McLaughlin <james@mclgh.net>
2024-07-30 22:29:11 +0100

    add codon dataload script

---
+++ scripts/dataload_codon.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+export GREBI_HOME=/nfs/production/parkinso/spot/grebi
+export GREBI_TMP=/hps/nobackup/parkinso/spot/grebi/tmp
+export GREBI_CONFIG=ebi
+export GREBI_IS_EBI=true
+export GREBI_TIMESTAMP=$(date +%Y_%m_%d__%H_%M)
+module load nextflow-22.10.1-gcc-11.2.0-ju5saqw
+module load python
+export PYTHONPATH="/homes/spotbot/.local/lib/python3.6/site-packages:$PYTHONPATH"
+cd /hps/nobackup/parkinso/spot/grebi/
+export PYTHONUNBUFFERED=true
+srun -p datamover --time 1:0:0 --mem 8g bash -c "rm -rf /nfs/public/rw/ontoapps/grebi/staging && mkdir /nfs/public/rw/ontoapps/grebi/staging"
+srun --time 23:0:0 --mem 8g bash -c "rm -rf work tmp && python3 ${GREBI_HOME}/scripts/dataload_codon.py"
+
+
