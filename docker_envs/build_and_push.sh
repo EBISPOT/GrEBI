f8e5f08c1c3fc93f6fe266b36ceeed2a0d04a9c7
James McLaughlin <james@mclgh.net>
2024-07-30 22:28:23 +0100

    add python docker env

--- docker_envs/build_and_push.sh
+++ docker_envs/build_and_push.sh
@@ -4,8 +4,12 @@ set -e
 
 docker build -t ghcr.io/ebispot/grebi_solr_with_python:9.5.0 -f Dockerfile.solr_with_python .
 docker build -t ghcr.io/ebispot/grebi_neo4j_with_python:5.18.0 -f Dockerfile.neo4j_with_python .
+docker build -t ghcr.io/ebispot/grebi_python:3.11 -f Dockerfile.python .
 docker build -t ghcr.io/ebispot/rust_for_codon:1.74 -f Dockerfile.rust_for_codon .
 
 docker push ghcr.io/ebispot/grebi_solr_with_python:9.5.0
 docker push ghcr.io/ebispot/grebi_neo4j_with_python:5.18.0
+docker push ghcr.io/ebispot/grebi_python:3.11
 docker push ghcr.io/ebispot/rust_for_codon:1.74
+
+
