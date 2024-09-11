1102073b47f9399415a90f2378d54207174c2510
James McLaughlin <james@mclgh.net>
2024-09-11 15:56:47 +0100

    use separate nextflow work dirs for different subgraphs

--- scripts/dataload_codon.sh
+++ scripts/dataload_codon.sh
@@ -11,7 +11,7 @@ export PYTHONPATH="/homes/spotbot/.local/lib/python3.6/site-packages:$PYTHONPATH
 cd /hps/nobackup/parkinso/spot/grebi/
 export PYTHONUNBUFFERED=true
 srun -p datamover --time 1:0:0 --mem 8g bash -c "rm -rf /nfs/public/rw/ontoapps/grebi/staging && mkdir /nfs/public/rw/ontoapps/grebi/staging"
-srun --time 3-0:0:0 --mem 8g bash -c "rm -rf work tmp && python3 ${GREBI_HOME}/scripts/dataload_codon.py"
+srun --time 3-0:0:0 --mem 8g bash -c "rm -rf work* tmp && python3 ${GREBI_HOME}/scripts/dataload_codon.py"
 #srun --time 23:0:0 --mem 8g bash -c "python3 ${GREBI_HOME}/scripts/dataload_codon.py"
 
 
