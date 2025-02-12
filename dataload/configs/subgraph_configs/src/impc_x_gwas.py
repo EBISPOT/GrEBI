4dc538fcc010ed877e7af4312d8bdff160dbd857
James McLaughlin <james@mclgh.net>
2025-02-12 15:39:34 +0000

    add opentargets to impc x gwas subgraph

--- dataload/configs/subgraph_configs/src/impc_x_gwas.py
+++ dataload/configs/subgraph_configs/src/impc_x_gwas.py
@@ -10,7 +10,8 @@ config['datasource_configs'] = [
     "./configs/datasource_configs/ols.yaml",
     "./configs/datasource_configs/mondo_efo.yaml",
     "./configs/datasource_configs/monarch.yaml",
-    "./configs/datasource_configs/sssom.yaml"
+    "./configs/datasource_configs/sssom.yaml",
+    "./configs/datasource_configs/otar.yaml"
 ]
 
 if __name__ == '__main__':
