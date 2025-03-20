a333ac72b2a42d7e75b46317a1aca207861eacef
James McLaughlin <james@mclgh.net>
2025-03-20 22:19:50 +0000

    fix primekg ingest

--- dataload/01_ingest/primekg.py
+++ dataload/01_ingest/primekg.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 
 import pandas
 import sys
@@ -7,20 +8,37 @@ df = pandas.read_csv(sys.stdin, dtype=str)
 
 for row in df.to_dict(orient="records"):
 
-    x_id = row['x_source'] + ':' + row['x_id']
-    y_id = row['y_source'] + ':' + row['y_id']
+    x_source = row['x_source']
+    y_source = row['y_source']
+
+    if x_source == "NCBI":
+        x_source = "NCBIGene"
+    if y_source == "NCBI":
+        y_source = "NCBIGene"
+
+    x_id = x_source + ':' + row['x_id']
+    y_id = y_source + ':' + row['y_id']
 
     res = {
         'id': x_id,
-        'grebi:name': row['x_name']
+        'grebi:name': row['x_name'],
+        'grebi:type': 'biolink:Entity'
     }
 
-    res["primekg:" + row['relation']] = {
+    rel = "primekg:" + row['relation']
+
+    res[rel] = {
         'grebi:value': y_id,
-        'grebi:properties': {f"primekg:{key}": value for key, value in row.items()}
+        'grebi:properties': {f"primekg:{key}": [value] for key, value in row.items()}
     }
 
     print(json.dumps(res))
 
+    res2 = {
+        'id': y_id,
+        'grebi:name': row['y_name'],
+        'grebi:type': 'biolink:Entity'
+    }
+    print(json.dumps(res2))
 
     
