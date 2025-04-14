5eedb8d6e7b66cfc5a305430ab75b9dc37c24157
James McLaughlin <james@mclgh.net>
2025-04-14 11:47:49 +0100

    correct neo4j image tag

--- dataload/docker_envs/build_and_push.sh
+++ dataload/docker_envs/build_and_push.sh
@@ -3,12 +3,12 @@
 set -e
 
 docker build -t ghcr.io/ebispot/grebi_solr_with_extras:9.8.1 -f Dockerfile.solr_with_extras .
-docker build -t ghcr.io/ebispot/grebi_neo4j_with_extras:2025.03.0 -f Dockerfile.neo4j_with_extras .
+docker build -t ghcr.io/ebispot/grebi_neo4j_with_extras:2025.03.0-community -f Dockerfile.neo4j_with_extras .
 docker build -t ghcr.io/ebispot/grebi_python:3.11 -f Dockerfile.python .
 docker build -t ghcr.io/ebispot/rust_for_codon:1.79 -f Dockerfile.rust_for_codon .
 
 docker push ghcr.io/ebispot/grebi_solr_with_extras:9.8.1
-docker push ghcr.io/ebispot/grebi_neo4j_with_extras:2025.03.0
+docker push ghcr.io/ebispot/grebi_neo4j_with_extras:2025.03.0-community
 docker push ghcr.io/ebispot/grebi_python:3.11
 docker push ghcr.io/ebispot/rust_for_codon:1.79
 
