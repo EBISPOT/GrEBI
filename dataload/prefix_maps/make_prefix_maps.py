0ca1e8a5b3ecd6053c2fd2a2ddc57d6bdef0e8e5
James McLaughlin <james@mclgh.net>
2025-11-14 12:44:15 +0000

    fix incorrect prefixes in prefix map

--- dataload/prefix_maps/make_prefix_maps.py
+++ dataload/prefix_maps/make_prefix_maps.py
@@ -25,28 +25,18 @@ for uri_prefix, prefix in c.reverse_prefix_map.items():
 
 with open('extra_prefixes.json', 'r') as extra:
     j = json.load(extra)
-    for prefix, uri_prefix in j.items():
-        if prefix in normalise:
-            norm_prefix = normalise[prefix]
+    for left, right in j.items():
+        if left in normalise:
+            if right in normalise:
+                if normalise[left] != normalise[right]:
+                    raise ValueError(f'Conflict in extra prefixes for {left} and {right}')
+            else:
+                normalise[right] = normalise[left]
+        elif right in normalise:
+            normalise[left] = normalise[right]
         else:
-            norm_prefix = prefix
-        if uri_prefix in normalise:
-            norm_uri_prefix = normalise[uri_prefix]
-        else:
-            norm_uri_prefix = uri_prefix
-        if not uri_prefix in compact:
-            compact[uri_prefix] = norm_prefix
-        if not uri_prefix in normalise:
-            normalise[uri_prefix] = norm_prefix
-        if not prefix in expand:
-            expand[prefix] = norm_uri_prefix
-        if not prefix in normalise:
-            normalise[prefix] = norm_prefix
+            normalise[left] = right
 
-with open('prefix_map_compact.json', 'w') as outfile:
-    json.dump(compact, outfile, indent=2)
-with open('prefix_map_expand.json', 'w') as outfile:
-    json.dump(expand, outfile, indent=2)
 with open('prefix_map_normalise.json', 'w') as outfile:
     json.dump(normalise, outfile, indent=2)
 
