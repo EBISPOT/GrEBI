a744e585ba8b59384e1212ae5c923e1cc3e5a7c7
James McLaughlin <james@mclgh.net>
2025-12-17 10:44:01 +0000

    change to pca model for nemotron

--- dataload/00_fetch_data/ols_embeddings/fetch.sh
+++ dataload/00_fetch_data/ols_embeddings/fetch.sh
@@ -3,6 +3,6 @@
 rm -f text-embedding-3-small.parquet llama-embed-nemotron-8b.parquet
 
 wget https://ftp.ebi.ac.uk/pub/databases/spot/ols_embeddings/text-embedding-3-small.parquet 
-wget https://ftp.ebi.ac.uk/pub/databases/spot/ols_embeddings/llama-embed-nemotron-8b.parquet
+wget https://ftp.ebi.ac.uk/pub/databases/spot/ols_embeddings/llama-embed-nemotron-8b_pca512.parquet
 
 
