aa793f194bd7ea7c9d457523cc82b26dccf65ae4
James McLaughlin <james@mclgh.net>
2026-03-01 17:18:05 +0000

    add disease_ontologies subgraph

---
+++ configs/subgraph_configs/src/disease_ontologies.py
@@ -0,0 +1,18 @@
+
+
+from shared import config
+
+config['id'] = 'DiseaseOntologies'
+config['name'] = 'Disease Ontologies (EFO, MONDO, DOID)'
+config['datasource_configs'] = [
+    "./configs/datasource_configs/ols_efo.yaml",
+    "./configs/datasource_configs/ols_mondo.yaml",
+    "./configs/datasource_configs/ols_doid.yaml",
+    "./configs/datasource_configs/embeddings__llama-embed-nemotron-8b.yaml",
+    "./configs/datasource_configs/embeddings__text-embedding-3-small.yaml"
+]
+
+if __name__ == '__main__':
+    import json
+    print(json.dumps(config, indent=2))
+
